6ae31d06b22ca821aa605ac7fd2bd72312d50c04
Racso01 <oscarmbarter@gmail.com>
2019-06-27 22:13:43 +0100

    init commit

---
+++ HelloWorldCS/Decisions/Decisions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Decisions
+{
+    class IfStatements
+    {
+        static void Main(string[] args)
+        {
+            //local varable definition
+            int a = 10;
+
+            //check boolean condition using if statement
+            if (a < 20)
+            {
+                Console.WriteLine("a is less than 20");
+            }
+            Console.WriteLine("value of a is : {0}", a);
+            Console.ReadKey();
+        }
+    }
+}
